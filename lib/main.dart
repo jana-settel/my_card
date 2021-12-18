365fc2e3c97ea1243c73684b21fb8b288c0dbebc
Jana Settel <zettelyana@gmail.com>
2021-12-18 19:30:23 +0100

    Add rows, icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +12,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xFFE9BF98),
+        backgroundColor: Color(0xFF413939),
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
-              radius: 50.0,
+              radius: 70.0,
               backgroundImage: AssetImage('images/jana.JPG'),
               //backgroundColor: Color.fromRGBO(168, 205, 239, 100),
               //backgroundColor: Color(0xFFE89F5B).withOpacity(0.5),
@@ -24,15 +27,76 @@ class MyApp extends StatelessWidget {
               'Jana Settel',
               style: TextStyle(
                   fontFamily: 'Fuzzybubbles',
-                  fontSize: 40.0,
-                  color: Color(0xFF945252)),
+                  fontSize: 45.0,
+                  color: Colors.white54),
             ),
             Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
-                fontSize: 25.0,
-                color: Colors.brown,
-                fontFamily: 'Luxurious Script',
+                fontSize: 17.0,
+                color: Color(0xFF9A8A8A),
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Color(0xFF9A8A8A),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+              color: Color(0xFFD1C3C3),
+              child: Padding(
+                padding: EdgeInsets.all(9.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 25.0,
+                      color: Color(0xFF413939),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+49 49595 449500',
+                      style: TextStyle(
+                        color: Color(0xFF413939),
+                        fontSize: 20.0,
+                        fontFamily: 'Fuzzybubbles',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+              color: Color(0xFFD1C3C3),
+              child: Padding(
+                padding: EdgeInsets.all(9.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 25.0,
+                      color: Color(0xFF413939),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'yanayana@inbox.com',
+                      style: TextStyle(
+                        color: Color(0xFF413939),
+                        fontSize: 20.0,
+                        fontFamily: 'Fuzzybubbles',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
